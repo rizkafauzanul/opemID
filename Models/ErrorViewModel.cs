8b19be689a63cc73b815044a800bc031dad89212
rfi <rizkafauzanul@RFI-NB.localdomain>
2021-09-01 16:15:33 +0700

    1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _1_1_MyOrg.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
